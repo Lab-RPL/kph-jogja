808de21279ed5df3397bafda2bfefc19e4bc0c83
VhyanichaDyan <vhyanichadyan@gmail.com>
2023-08-22 15:20:20 +0700

    hhbk ichak

--- database/migrations/2023_08_22_070146_create_hhbk.php
+++ database/migrations/2023_08_22_070146_create_hhbk.php
@@ -12,7 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('hhbk', function (Blueprint $table) {
-            $table->id();
+            $table->id('id_hhbk')->autoIncrement()->primary();
+
+            $table->varchar('nama_wisata');
+            $table->text('lokasi_wisata');
+            $table->text('atraksi_wisata');
             $table->timestamps();
         });
     }
