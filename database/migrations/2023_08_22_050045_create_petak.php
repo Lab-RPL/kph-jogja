b9c0d26405f8b50aac1ca3295642847c1436493a
ZeeStillLearning <fauziyananda@gmail.com>
2023-08-22 15:28:46 +0700

    69

--- database/migrations/2023_08_22_050045_create_petak.php
+++ database/migrations/2023_08_22_050045_create_petak.php
@@ -12,8 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('petak', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id('id_ptk');
+
+            $table->integer('nomor_ptk');
+            $table->decimal('luas_rph');
+            $table->string('potensi_ptk');
+            $table->unsignedBigInteger('id_rph');
+            $table->foreign('id_rph')->references('id_rph')->on('rph');
         });
     }
 
