4296466fea4889757a753915a05771ce6cc8f1b5
jackLokal <fransjuan25@gmail.com>
2023-09-12 16:03:32 +0700

    Membuat Model hhbk

---
+++ app/Models/hhbk.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class hhbk extends Model
+{
+    use HasFactory;
+
+    protected $table = 'hhbk';
+    protected $primaryKey = 'id_hhbk';
+    protected $fillable = ['idhh', 'nama_wisata', 'lokasi_wisata', 'atraksi_wisata'];
+}
