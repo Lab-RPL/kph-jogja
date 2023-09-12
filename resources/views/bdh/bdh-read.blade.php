940ea7098470792fc10026d95352306aa9e1601f
LilAlamin <ronaldramadhani15@gmail.com>
2023-09-12 11:29:28 +0700

    table dirubah menjadi tabledata

--- resources/views/bdh/bdh-read.blade.php
+++ resources/views/bdh/bdh-read.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.main')
 @section('content')
+    <link rel="stylesheet" href="//cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
@@ -33,9 +34,17 @@
                             @endif
                         @endforeach
                     </tbody>
-                </table> 
-              {{ $data->links() }}
+                </table>
+                {{ $data->links() }}
             </form>
         </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="//cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script>
+            $(document).ready(function(){
+                $('#tabelData').DataTable();
+            })
+    </script>
 @endsection
