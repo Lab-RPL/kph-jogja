1cb6f05f0ad5c69e618d1a6cf66f3584105ae66d
LilAlamin <ronaldramadhani15@gmail.com>
2023-09-14 13:48:43 +0700

    role not admin tidak bisa akses admin

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -1,10 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
 
 class adminController extends Controller
 {
     //
+    public function index()
+    {
+        // Pastikan pengguna telah masuk dan bergolong 'admin' sebelum mengizinkan akses.
+        if (Auth::check() && Auth::user()->user_type == 'admin') {
+            // Tampilkan halaman admin atau lakukan apa pun yang Anda inginkan
+            return view('admin.admin');
+        }
+
+        return redirect('/dashboard')->with('error', 'Anda tidak memiliki akses ke halaman admin.');
+    }
 }
