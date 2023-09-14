cc2a92e3a71920dfe87fb791ea30b8a9ce15a3c1
jackLokal <fransjuan25@gmail.com>
2023-09-14 13:29:18 +0700

    Memberi tampilan pada "edit-admin"

---
+++ resources/views/admin/edit-admin.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.main')
+
+@section('content')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <form action="{{ route('potensi.update', ['id' => $hhbk->id_hhbk]) }}" method="POST">
+        @csrf
+        @method('put')
+        <div class="garis">
+            <div class="border-list">
+                <h2>ADMIN</h2>
+                <p>Pemantauan Potensi dan Gangguan Sumber Daya Hutan di Yogyakarta</p>
+                {{-- <input type="hidden" name="id_hhbk" value="{{ $hhbk->id_hhbk }}"> --}}
+                <table id="tabelData">
+                    <tr></tr>
+                    <td><label for="name">Username</label></td>
+                    <td><input type="text" id="name" name="name" value="{{}}"></td>
+                    </tr>
+                    {{-- <tr>
+                        <td><label for="pass">Password</label></td>
+                        <td><input type="text" id="pass" name="password" value="{{}}"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="kon-pass">Konfirmasi Password</label></td>
+                        <td><input type="text" id="kon-pass" name="" value="{{}}"></td>
+                    </tr> --}}
+                </table>
+                <div style="display: flex; justify-content: space-between; margin-top: 15px;">
+                    <a class="btn btn-warning" style="color: white" href="/admin">Kembali</a>
+                    <button class="btn btn-primary" style="color: white" type="submit">Edit Data</button>
+                </div>
+            </div>
+        </div>
+    </form>
+@endsection
