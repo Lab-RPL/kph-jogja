43d224a3340c4e47ac6c241a75b35f8415adf323
jackLokal <fransjuan25@gmail.com>
2023-09-14 13:10:34 +0700

    Menambah file yang berkaitan dengan"admin"

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -1,35 +1,35 @@
 @extends('layouts.main')
 @section('content')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
-  <div class="garis">
-    <div class="border-list">
-      <h2>DATA ADMIN</h2>
-      <p>Pemantauan Potensi dan Gangguan Sumber Daya Hutan di Yogyakarta</p>
-      <form>
-        <table id="tabelData">
-          <thead>
-            <tr>
-              <th>Username</th>
-              <th>Email</th>
-              <th>Password</th>
-            </tr>
-          </thead>
-          <tbody>
-            <!-- Isi data dinamis di sini -->
-          </tbody>
-        </table>
-        <div id="pagination" class="pagination">
-          <!-- Pagination dinamis di sini -->
+    <div class="garis">
+        <div class="border-list">
+            <h2>ADMIN</h2>
+            <p>Pemantauan Potensi dan Gangguan Sumber Daya Hutan di Yogyakarta</p>
+            <form>
+                <table id="tabelData" class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Username</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+                <div style="display: flex; justify-content: flex-end; margin-top:15px;" class="nav-item">
+                    <a class="btn btn-primary" style="color: white" href="tambah-admin">Tambah User</a>
+                </div>
+            </form>
         </div>
-        <div class="button-container">
-          <button type="submit">Tambah Data</button>
-        </div>
-      </form>
     </div>
-  </div>
 
- {{-- pagination js --}}
- <script>
+    {{-- pagination js 
+    <script>
     const tabelData = document.getElementById("tabelData");
     const tbody = tabelData.querySelector("tbody");
     const pagination = document.getElementById("pagination");
@@ -192,6 +192,6 @@ function createPagination(items, activePage = 1) {
 
 displayData(data);
 createPagination(data);
-  </script>
-
+  </script> 
+  pagination js --}}
 @endsection
