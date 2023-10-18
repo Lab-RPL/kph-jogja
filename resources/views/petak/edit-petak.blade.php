3f1b98ac0e3db2c3340f056a2b74f611d66131a5
jackLokal <fransjuan25@gmail.com>
2023-10-18 16:08:19 +0700

    Fungsi edit pada petak dapat digunakan....
    Namun value jenis tegakan tidak muncul saat membuka page edit

--- resources/views/petak/edit-petak.blade.php
+++ resources/views/petak/edit-petak.blade.php
@@ -21,6 +21,8 @@
                                 @if ($rph->IsDelete == 0 && $rph->id_rph == $petak->id_rph)
                                     <input value=" {{ $rph->nama_rph }}" id="tambah-rph" type="text" name="nama_rph"
                                         disabled>
+                                        <input value="{{ $rph->id_rph }}" id="tambah-rph" type="hidden" name="id_rph">
+
                                 @break
                             @endif
                         @endforeach
@@ -44,22 +46,105 @@
                 </tr>
                 <tr>
                     <td><label for="potensi-ptk">Potensi Petak</label></td>
-                    <td><input type="text" id="potensi-ptk" name="potensi_ptk" value="{{ $petak->potensi_ptk }}">
+                    <td>
+                        <select id="potensi-ptk" name="potensi_ptk" required>
+                            <option value="" disabled hidden>Pilih Potensi</option>
+                            <option value="0" {{ $petak->potensi_ptk == 0 ? 'selected' : '' }}>Hutan Kayu</option>
+                            <option value="1" {{ $petak->potensi_ptk == 1 ? 'selected' : '' }}>Hutan Bukan Kayu
+                            </option>
+                        </select>
                     </td>
                 </tr>
-            </table>
-            <div style="display: flex; justify-content: space-between; margin-top: 15px;">
-                <a class="btn btn-warning" style="color: white" href="/data-bdh" onclick="return goBack();">Kembali</a>
+                <tr>
+                    <td><label for="jenis_tgk">Jenis Tegakan</label></td>
+                    <td>
+                        <select id="jenis_tgk" name="id_tgk">
+                            {{-- <input value="{{ $petak->id_hhk }}" id="edit-tgk" type="hidden" name="id_hhk">
+                            <input value="{{ $petak->id_hhbk }}" id="edit-tgk" type="hidden" name="id_hhbk"> --}}
+                        </select>
+                    </td>
+                </tr>
 
-                <script>
-                    function goBack() {
-                        window.history.back();
-                        return false;
-                    }
-                </script>
-                <button class="btn btn-primary" style="color: white" type="submit">Edit Data</button>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+                {{-- <tr>
+                    <td><label for="jenis_tgk">Jenis Tegakan</label></td>
+                    <td>
+                        <select id="jenis_tgk" name="id_tgk">
+                            <option value=""></option>
+                            @foreach ($petak as $jenisTgkOption)
+                                <option value="{{ $jenisTgkOption->id }}"
+                                    {{ $jenisTgkOption->id == $petak->id_tgk ? 'selected' : '' }}>
+                                    {{ $jenisTgkOption->jenis_tgk }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </td>
+                </tr> --}}
+
+            </table>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+            {{-- <script>
+                $("#potensi-ptk").change(function() {
+                    var type = $(this).val();
+                    var url = '{{ route('petak.getJenisTgk', ':type') }}';
+                    url = url.replace(':type', type);
+
+                    $.ajax({
+                        url: url,
+                        type: 'GET',
+                        success: function(data) {
+                            var $jenisTgk = $('#jenis_tgk');
+
+                            $jenisTgk.empty(); // Empty current options
+
+                            for (var i = 0; i < data.length; i++) {
+                                var idValue = type == 0 ? data[i].id_hhk : data[i].id_hhbk;
+                                $jenisTgk.append('<option value=' + idValue + '>' + data[i].jenis_tgk +
+                                    '</option>');
+                            }
+                        }
+                    });
+                });
+            </script> --}}
+
+            <div style="display: flex; justify-content: space-between;" class="mt-4">
+                <a class="btn btn-warning" style="color: white; font-weight:bold;" onclick="return goBack()">Kembali</a>
+                <button class="btn btn-primary"
+                    style="background-color: #9CC589; border: 1px solid #9CC589; color: #ffffff; font-weight: bold"
+                    type="submit">Submit</button>
             </div>
         </div>
     </div>
 </form>
+<script>
+    $("#potensi-ptk").change(function() {
+        var type = $(this).val();
+        var url = '{{ route('petak.getJenisTgk', ':type') }}';
+        url = url.replace(':type', type);
+
+        $.ajax({
+            url: url,
+            type: 'GET',
+            success: function(data) {
+                var $jenisTgk = $('#jenis_tgk');
+                $jenisTgk.empty(); // Kosongkan opsi saat ini
+
+                for (var i = 0; i < data.length; i++) {
+                    var optionValue = type === '0' ? data[i].id_hhk : data[i].id_hhbk;
+                    var optionText = data[i].jenis_tgk;
+
+                    $jenisTgk.append('<option value="' + optionValue + '">' + optionText +
+                        '</option>');
+                }
+            }
+        });
+    });
+
+    function goBack() {
+        window.history.back();
+        return false;
+    }
+</script>
 @endsection
